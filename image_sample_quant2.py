98118a34134fa0c09ada036a458eac83cc849c3e
Michael Heath <heaths19@students.ecu.edu>
2020-09-03 19:56:08 -0400

    Update image_sample_quant2.py

--- image_sample_quant2.py
+++ image_sample_quant2.py
@@ -122,8 +122,6 @@ def prev_img(event):
     update_window(imgtk, tex)
 
 
-'''might get changed'''
-
 def shrink_NN(event):
     nearest_neighbor(0.5)
 
@@ -132,10 +130,6 @@ def shrink_bicubic(event):
 
 def shrink_bilinear(event):
     bilinear(0.5)
-    
-def shrink_linear(event):
-    linear(0.5)
-    
 
 def increase_NN(event):
     nearest_neighbor(2)
@@ -145,9 +139,6 @@ def increase_bicubic(event):
 
 def increase_bilinear(event):
     bilinear(2)
-    
-def increase_linear(event):
-    linear_inc(2)
 
 def nearest_neighbor(factor=0.5):
     global count
@@ -179,55 +170,43 @@ def bilinear(factor=0.5):
     #Update the display
     update_window(imgtk, tex)
 
-# Use linear interpolation to fill in missing pixels when shrinking or increasing by factor of 2
-def linear_inc(factor  = 2):
+
+# Enlarge image by factor of 2 using linear interpolation
+def enlarge_linear(event):
     global current_disp
-    global columns
-    global rows
-    global count
-    
-    # Take current image and make into a np.array
-    old_image = np.array(current_disp)
+    image = opencv_img(count)
+
+    # print("The shape of the image is " + current_disp.shape[0] + " by " + current_disp.shape[1])
+    print("The shape of the image is " + str(image.shape[0]) + " by " + str(image.shape[1]))
+
     # We make a new matrix (multi-dim array) of all zeros twice the width and height that holds RGB
-    new_img = np.zeros((2*int(rows[count]),2*int(columns[1]), 3),dtype=np.uint8)
-    
+    # enlarged_img = np.zeros((2 * current_disp.shape[0], 2 * current_disp.shape[1], 3), dtype=np.uint8)
+    enlarged_img = np.zeros((2 * image.shape[0], 2 * image.shape[1], 3), dtype=np.uint8)
+
     # For each row, go to each column position and if the col is divisible by 2, copy old img values
     # otherwise use linear interpolation with left and right spatial coordiante RGB values
-    for i in range(2*int(columns[count])):
-        if i%2 == 0:
-            for j in range(2*int(rows[count])):
-                if j%2 == 0:
-                    new_img[i,j] = old_image[i,j//2]
-                else:
-                    new_img[i,j] = (1/2)*current_disp[i,j//2] + (1/2)*current_disp[i,j//2+1]             
-                
-    # For each column, go to each row position and if the row is divisible by 2, copy old img values
-    # otherwise use linear interpolation with above and below spatial coordiante RGB values            
-    for j in range(2*int(rows[count])):
-        if j%2 == 0:
-            for i in range(2*int(columns[count])):
-                if i%2 ==0:
-                    new_img[i,j] = current_disp[i/2,j]
-                else:
-                    new_img[i,j] = (1/2)*current_disp[i//2-1,j] + (1/2)*current_disp[i//2+1,j]              
-    
-    # We now use the diagonal line to fill in the pixels in the gaps left 
-    for i in range(2*int(columns[count])):
-        if i%2 != 0:
-            for j in range(2*int(rows[count])):
-                if j%2 != 0:
-                    new_img[i,j] = (1/2)*current_disp[i//2 + 1, j//2 - 1] + (1/2)*current_disp[i//2 -1, j//2 + 1]
-           
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            enlarged_img[i*2, j*2] = image[i, j]
+            if j < image.shape[1] - 1:
+                enlarged_img[i*2, j*2 + 1] = (1 / 2) * image[i, j] + (1 / 2) * image[i, j + 1]
+            else:
+                enlarged_img[i*2, j*2 + 1] = image[i, j]
 
-    # To make an image from the array
-    img =  ImageTk.PhotoImage(image=Image.fromarray(new_img))                        
-    imgtk = convert_img(img)
+    # For each column, go to each row position and if the row is divisible by 2, copy old img values
+    # otherwise use linear interpolation with above and below spatial coordiante RGB values
+    # for j in range(current_disp.shape[1] * 2):
+    for j in range(enlarged_img.shape[1]):
+        for i in range(1, enlarged_img.shape[0] - 2, 2):
+            enlarged_img[i, j] = (1/2)*enlarged_img[i-1, j] + (1/2)*enlarged_img[i+1, j]
+        enlarged_img[enlarged_img.shape[0] - 1, j] = enlarged_img[enlarged_img.shape[0] - 2, j]
+
+    enlarged_img = enlarged_img[0:int(enlarged_img.shape[0]*2), 0:int(enlarged_img.shape[1]*2)]
+    imgtk = convert_img(enlarged_img)
     tex = extract_meta()
-    update_window(imgtk,tex)
-   
-    
-    
-    
+    update_window(imgtk, tex)
+
+
 #Exit the program
 def quit_img(event):
     root.destroy() #Kill the display
@@ -343,21 +322,12 @@ def main():
     )
     btn_bilinear.grid(row = 1, column = 2)
     btn_bilinear.bind('<ButtonRelease-1>', shrink_bilinear)
-    
-    # Button for Linear
-    btn_linear_shrink = Button(
-        master = frame,
-        text = "Linear, shrink",
-        underline = 2
-    )
-    btn_linear_shrink.grid(row = 1, column = 3)
-    btn_linear_shrink.bind('<ButtonRelease-1>', shrink_linear)
 
-    # Button for Nearest neighbor increase
+      # Button for Nearest neighbor increase
     btn_nearest = Button(
         master = frame,
         text = "Nearest Neighbor, increase",
-        underline = 0
+        underline = 1
     )
     btn_nearest.grid(row = 2, column = 0)
     btn_nearest.bind('<ButtonRelease-1>', increase_NN)
@@ -370,7 +340,7 @@ def main():
     )
     btn_bicubic.grid(row = 2, column = 1)
     btn_bicubic.bind('<ButtonRelease-1>', increase_bicubic)
-    
+
     # Button for Bilinear increase
     btn_bilinear = Button(
         master = frame,
@@ -379,15 +349,13 @@ def main():
     )
     btn_bilinear.grid(row = 2, column = 2)
     btn_bilinear.bind('<ButtonRelease-1>', increase_bilinear)
-    
-    # Button for Linear increase
-    btn_linear_inc = Button(
+
+    btn_linear = Button(
         master = frame,
         text = "Linear, increase",
-        underline = 2
     )
-    btn_linear_inc.grid(row = 2, column = 2)
-    btn_linear_inc.bind('<ButtonRelease-1>', increase_linear)
+    btn_linear.grid(row = 3, column = 0)
+    btn_linear.bind('<ButtonRelease-1>', enlarge_linear)
 
     # Bind all the required keys to functions
     root.bind('<n>', next_img)
